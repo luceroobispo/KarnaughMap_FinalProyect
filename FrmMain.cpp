681462834b4f5ce5bd9c9dce2122b4065d7fb47b
Lucero Obispo <91815464+luceroobispo@users.noreply.github.com>
2022-05-15 21:19:18 -0500

    Create FrmMain.cpp

---
+++ FrmMain.cpp
@@ -0,0 +1,10 @@
+#include "FrmMain.h"
+using namespace System;
+using namespace Windows::Forms;
+using namespace Grupo02Karnagaugh;
+
+int main() {
+	Application::EnableVisualStyles();
+	Application::Run(gcnew Grupo02Karnagaugh::FrmMain);
+	return 0;
+}
